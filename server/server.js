210682732981b5709107362515001a2683f56e50
Jonnie Möller <scarabeo85@msn.com>
2024-04-17 17:48:38 +0200

    Revert "test"
    This reverts commit 27bf6ecf84c9fbd6508b8e2cc0be052c9620e182.

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const validateRoutes = require("./routes/validateRoutes")
 const app = express();
 app.use(
   cors({
-    origin: "https://beardcrafter.netlify.app/",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
